5cf8866f29eae70ea848ff8b5e6960ab8797b688
khush patel <zkac198@live.rhul.ac.uk>
2025-02-10 17:08:04 +0000

    grid made

--- Maze_generator.py
+++ Maze_generator.py
@@ -1,19 +1,48 @@
 import pygame
-import pygame.display 
+import pygame.display
+import pygame.draw 
 
 class Mazegenerator:
 
-    def __init__(self):
+    def __init__(self,rows=5,columns=5):
         
         pygame.init()
 
         self.height = 500
         self.width = 700
-        self.background_colour = (0,255,0)
+        self.background_colour = (157,146,245)
 
-        window = pygame.display.set_mode((self.width,self.height))
-        window.fill(self.background_colour)
-    
+        self.window = pygame.display.set_mode((self.width,self.height))
+        self.window.fill(self.background_colour)
+
+        self.cells = []
+        self.cell_walls = []
+        
+        self.rows = rows
+        self.columns = columns
+
+        self.block_size = 50
+
+        self.start_location = (20,20)
+        self.border_colour = (255,0,0)
+
+    def draw_grid(self):
+
+        starting_x, starting_y = self.start_location
+        starting_y -= self.block_size
+
+        for i in range(self.rows):
+            starting_y += self.block_size
+            for j in range(self.columns):
+                pygame.draw.rect(self.window,self.border_colour,
+                (starting_x + (j*self.block_size), starting_y,self.block_size,self.block_size),width=2)
+            
+
+
+            
+
+
+
     def run(self):
         
         running = True
@@ -21,5 +50,6 @@ class Mazegenerator:
             pygame.display.update()
 
 
-mg = Mazegenerator()
+mg = Mazegenerator(rows=5,columns=6)
+mg.draw_grid()
 mg.run()
