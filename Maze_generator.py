4e0c0a742b9db0e1149829161ca4588f247248cf
khush patel <zkac198@live.rhul.ac.uk>
2025-02-13 22:41:45 +0000

    available walls logicial error fixed

--- Maze_generator.py
+++ Maze_generator.py
@@ -22,7 +22,7 @@ class Cell:
         self.line_width = line_width
         self.border_colour = border_colour
         self.visited = False
-
+    
     def draw_cell(self, window):
         self.window = window
         pygame.draw.rect(self.window,self.border_colour,(self.x, self.y,self.size,self.size),width = self.line_width)
@@ -45,7 +45,7 @@ class Cell:
             starting_coordinate = (self.x + self.size, self.y)
             ending_coordinate = (self.x + self.size, self.y + self.size)
 
-        pygame.draw.line(self.window,(157,146,245),starting_coordinate, ending_coordinate,width = self.line_width)
+        pygame.draw.line(self.window,(157,146,245),starting_coordinate, ending_coordinate,width = self.line_width*2)
 
     def get_coordinates(self):
         return (self.x, self.y)
@@ -97,64 +97,55 @@ class Mazegenerator:
 
                 current_x = starting_x + (j*self.block_size)
 
-                new_cell = Cell((current_x,starting_y),size=self.block_size, line_width=2)
+                new_cell = Cell((current_x,starting_y),size=self.block_size, line_width=1)
 
                 new_cell.draw_cell(self.window)
                 self.cells.append(new_cell)
 
-                #pygame.draw.rect(self.window,self.border_colour,
-                #(current_x, starting_y,self.block_size,self.block_size),width=2)
-
-                #self.cells.append((current_x, starting_y))
-                
-                #show the grid being made
-                #pygame.display.update()
+                pygame.display.update()
                 #time.sleep(0.5)
     
     def generate_maze(self):
         
-        visited = []
-        start_position = random.randint(0,len(self.cells))
-        starting_cell = self.cells[0]
+        stack = []
 
-        self.available_walls(starting_cell)
-        print(f"{starting_cell.get_coordinates() = }, {starting_cell.walls = }")
-        #while len(visited) != len(self.cells):
-            
+        oc = 4
 
-        #    north = 1
-        #    east = 1
-        #    south = 1
-         #   west = 1
-
-
+        self.cells[oc].border_colour = (0,255,0)
+        self.cells[oc].draw_cell(self.window)
+        print(self.available_walls(self.cells[oc]))
+        
     def available_walls(self,current_cell):
 
         x,y = current_cell.get_coordinates()
 
         start_x, start_y = self.start_location
 
-        x_limit = self.rows * self.block_size + start_x
-        y_limit = self.columns * self.block_size + start_y
+        x_limit = (self.columns - 1) * self.block_size + start_x
+        y_limit = (self.rows - 1) * self.block_size + start_y
+
+
+        print(f"{x_limit = }, {y_limit = }")
+        print(f"{x = } ,{y = }")
 
         cell_walls = current_cell.walls
         wall_keys = list(cell_walls.keys())
-        
-        if x == start_x:
-            #remove north
-            cell_walls[wall_keys[0]] = False
 
         if y == start_y:
-            #remove east
+            #remove North
+            cell_walls[wall_keys[0]] = False
+
+        if x == x_limit:
+            #remove East
             cell_walls[wall_keys[1]] = False
-        
+
         if y == y_limit:
             #remove south
             cell_walls[wall_keys[2]] = False
             
-        if x == x_limit:
+        if x == start_x:
             #remove west
-            cell_walls[wall_keys[4]] = False
+            cell_walls[wall_keys[3]] = False
             
 
         return cell_walls
@@ -171,9 +162,7 @@ class Mazegenerator:
             pygame.display.update()
 
 
-mg = Mazegenerator(rows=4,columns=5)
+mg = Mazegenerator(rows=7,columns=5)
 mg.draw_grid()
-#mg.get_details()
-#mg.create_wall((40,100),(100,30))
 mg.generate_maze()
 mg.run()
